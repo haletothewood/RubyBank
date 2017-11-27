82062b1d595d74691fa80506b60678622299fda2
David Halewood <davidnhalewood@hotmail.com>
2017-11-27 13:59:33 +0000

    Adding remaining balance to transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,7 @@
 class Transaction
-  attr_reader :date, :type
+  attr_reader :date, :type, :balance
 
-  def initialize(type)
-    @date = Date.today
-    @type = type
+  def initialize(type, balance)
+    @date, @type, @balance = Date.today, type, balance
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,6 +1,7 @@
 describe Transaction do
   type = 'debit'
-  let(:transaction) { Transaction.new(type) }
+  balance = 1000
+  let(:transaction) { Transaction.new(type, balance) }
 
   it 'is created with a date' do
     expect(transaction.date).to eq Date.today
@@ -8,5 +9,9 @@ describe Transaction do
 
   it 'is created with a transaction type' do
     expect(transaction.type).to eq 'debit'
+  end
+
+  it 'is created with a remaining balance' do
+    expect(transaction.balance).to eq 1000
   end
 end
