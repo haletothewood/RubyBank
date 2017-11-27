ef1c06fc694297c6c919a48ef32c3c658187da06
David Halewood <davidnhalewood@hotmail.com>
2017-11-27 16:52:12 +0000

    Adding ability to print a statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,18 +1,24 @@
 require 'date'
 
 describe Statement do
-
-  let(:transaction) {double :transaction, 
-                            date: Date.today, 
-                            type: 'debit', 
-                            amount: 500, 
-                            balance: 1500
-                    }
+  let(:transaction) do
+    double :transaction,  date: Date.today,
+                          type: 'debit',
+                          amount: 500,
+                          balance: 1500
+  end
   let(:history) { double :history, transactions: [transaction] }
   let(:statement) { Statement.new(history) }
 
   it 'is created with a account history' do
     expect(statement.history).to eq history
   end
+
+  context '#print_statement' do
+    it 'prints the account information for the user to see' do
+      expect { statement.print_statement }.to output(
+        "date || credit || debit || balance\n2017-11-27 || || 500 || 1500\n"
+      ).to_stdout
+    end
+  end
 end
-    
