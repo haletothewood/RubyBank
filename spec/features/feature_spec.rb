9237320b29f45414a2f56aecbb467af0a3eadc5d
David Halewood <davidnhalewood@hotmail.com>
2017-11-27 16:04:04 +0000

    Adding a feature spec

---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,16 @@
+require 'account'
+require 'history'
+require 'transaction'
+
+describe 'Account' do
+  account = Account.new(0, Transaction, History.new)
+  it 'lets you make a deposit' do
+    account.deposit(300)
+  end
+  it 'lets you withdraw money' do
+    account.withdraw(100)
+  end
+  it 'won\'t let you withdraw more than you have' do
+    expect { account.withdraw(400) }.to raise_error
+  end
+end
