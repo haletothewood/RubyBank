277ccebd490f802014b6d12465ba97eb5ad6ab00
David Halewood <davidnhalewood@hotmail.com>
2017-11-27 17:11:43 +0000

    Adding ability for ATM to show statement

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,12 @@
 class ATM
   attr_reader :account
 
-  def initialize(account)
+  def initialize(account, statement)
     @account = account
+    @statement = statement
+  end
+
+  def show_statement
+    @statement.print_statement
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,9 +5,15 @@ describe ATM do
   let(:transaction) { double :transaction, type: nil, balance: nil }
   let(:history) { double :history, transactions: [] }
   let(:account) { double :account, amount: 1000, transaction: transaction, history: history }
-  let(:atm) { ATM.new(account) }
+  let(:statement) { double :statement }
+  let(:atm) { ATM.new(account, statement) }
   
   it "is created with an account" do
     expect(atm.account).to be account
+  end
+
+  it "is can instruct a statement to print" do
+    expect(statement).to receive(:print_statement)
+    atm.show_statement
   end
 end
